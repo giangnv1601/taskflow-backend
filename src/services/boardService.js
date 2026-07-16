c4ff6ffc2a3aecdb559bcaecbea8e711fea7e971
Nguyen Van Giang <giang16012405@gmail.com>
2026-07-16 23:42:59 +0700

    API Create Board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,22 +1,28 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
     // Xử lý logic dữ liệu tùy đặc thù dự án
-    const newwBoard = {
+    const newBoard = {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
 
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    // ...
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log(createdBoard)
+
+    // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
 
     // Làm thêm các xử lý logic khác với các Collection khác tùy dặc thù dự án...vv
     // Bắn email, notification về cho admin khi có 1 cái board mới được tạo...vv
 
     // Trả kết quả về, trong Service luôn phải có return
-    return newwBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
